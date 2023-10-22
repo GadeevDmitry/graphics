536604672d852e7ab0443b0f67a68087f9cade4b
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-10-22 18:27:32 +0300

    tools: little changes

--- tools/include/tool.h
+++ tools/include/tool.h
@@ -19,10 +19,10 @@ public:
     explicit inline  tool_t();
     virtual  inline ~tool_t() {}
 
-    virtual void deactivate            () {}
-    virtual void paint_on_mouse_press  (render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &texture_off, const MOUSE_BUTTON_TYPE &btn) = 0;
-    virtual void paint_on_mouse_release(render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &texture_off, const MOUSE_BUTTON_TYPE &btn) = 0;
-    virtual void paint_on_mouse_move   (render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &textute_off, const vec2d             &off) = 0;
+    void virtual deactivate            () {}
+    void virtual paint_on_mouse_press  (render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &texture_off, const MOUSE_BUTTON_TYPE &btn) = 0;
+    void virtual paint_on_mouse_release(render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &texture_off, const MOUSE_BUTTON_TYPE &btn) = 0;
+    void virtual paint_on_mouse_move   (render_texture_t &perm, render_texture_t &temp, const color_t &color, const vec2d &textute_off, const vec2d             &off) = 0;
 };
 
 //--------------------------------------------------------------------------------------------------
--- tools/src/ellipse_tool.cpp
+++ tools/src/ellipse_tool.cpp
@@ -12,18 +12,16 @@ void ellipse_tool_t::draw_temp(render_texture_t &temp, const color_t &color)
     double y_scale = (dblcmp(x_size, 0) == 0) ? 0 : y_size / x_size;
     double x_scale = (dblcmp(y_size, 0) == 0) ? 0 : x_size / y_size;
 
+    temp.clear(color_t::Transparent);
+
     if (x_size < y_size)
     {
         circle_t circle(vec2d((start.x + last.x) / 2, (start.y + last.y) / 2), x_size / 2);
-
-        temp.clear(color_t::Transparent);
         temp.draw_circle(circle, color_t::Transparent, color, 1, vec2d(1, y_scale));
     }
     else
     {
         circle_t circle(vec2d((start.x + last.x) / 2, (start.y + last.y) / 2), y_size / 2);
-
-        temp.clear(color_t::Transparent);
         temp.draw_circle(circle, color_t::Transparent, color, 1, vec2d(x_scale, 1));
     }
 }
