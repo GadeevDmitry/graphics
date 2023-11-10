5eb7f65a518670775298d405ae74a441cfb0ba7d
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-11-10 13:57:27 +0300

    graphic: implement "font_t"

---
+++ graphic/font.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "graphic/font.h"
+
+//==================================================================================================
+
+const char *font_t::FONTS_FILES[] =
+{
+    "../../widget/fonts/red_seven.otf"
+};
+
+font_t font_t::FONTS[sizeof(font_t::FONTS_FILES) / sizeof(char *)] = {};
+
+//==================================================================================================
+
+void font_t::load_fonts()
+{
+    for (size_t i = 0; i * sizeof(char *) < sizeof(FONTS_FILES); ++i)
+    {
+        FONTS[i].load_from_file(FONTS_FILES[i]);
+    }
+};
+
+//--------------------------------------------------------------------------------------------------
+
+const font_t &font_t::get_font_by_name(const FONT_NAME_TYPE &name)
+{
+    if (name <= FONT_NAME_TYPE_UNKNOWN)
+    {
+        LOG_ERROR("undefined font name: %d\n", name);
+        return FONTS[FONT_NAME_TYPE_UNKNOWN + 1];
+    }
+
+    if (name >= FONT_NAME_TYPE_COUNT)
+    {
+        LOG_ERROR("undefined font name: %d\n", name);
+        return FONTS[FONT_NAME_TYPE_COUNT - 1];
+    }
+
+    return FONTS[name];
+}
---
+++ include/graphic/font.h
@@ -0,0 +1,81 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include "data_structs/include/log.h"
+#include <SFML/Graphics.hpp>
+
+//==================================================================================================
+
+class font_t
+{
+// types
+public:
+    enum FONT_NAME_TYPE
+    {
+        FONT_NAME_TYPE_UNKNOWN = -1,
+
+        RED_SEVEN,
+
+        FONT_NAME_TYPE_COUNT
+    };
+
+// static
+public:
+    static void          load_fonts      ();
+    static const font_t &get_font_by_name(const FONT_NAME_TYPE &name);
+private:
+    static font_t      FONTS[];
+    static const char *FONTS_FILES[];
+
+// member functions
+public:
+             inline font_t();
+    explicit inline font_t(const FONT_NAME_TYPE &font_name);
+    explicit inline font_t(const sf::Font &data);
+    explicit inline font_t(const char *filename);
+
+    bool inline load_from_file(const char *filename);
+
+// member data
+private:
+    sf::Font data;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+inline font_t::font_t():
+data()
+{}
+
+//--------------------------------------------------------------------------------------------------
+
+inline font_t::font_t(const FONT_NAME_TYPE &font_name):
+data()
+{
+    LOG_VERIFY(font_name > FONT_NAME_TYPE_UNKNOWN, ;);
+    LOG_VERIFY(font_name < FONT_NAME_TYPE_COUNT  , ;);
+    data = FONTS[font_name].data;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+inline font_t::font_t(const sf::Font &data_):
+data(data_)
+{}
+
+//--------------------------------------------------------------------------------------------------
+
+inline font_t::font_t(const char *filename):
+data()
+{
+    data.loadFromFile(filename);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+inline bool font_t::load_from_file(const char *filename)
+{
+    return data.loadFromFile(filename);
+}
+
+#endif // FONT_H
