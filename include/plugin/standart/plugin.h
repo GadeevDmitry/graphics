c32e98f3a9cb678cc2c63358e0a7c7f9ce4dbd56
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-12-05 22:15:17 +0300

    plugin: add "plugin.h"

---
+++ include/plugin/standart/plugin.h
@@ -0,0 +1,25 @@
+#ifndef PLUGIN_PLUGIN_H
+#define PLUGIN_PLUGIN_H
+
+#include <cinttypes>
+#include "Interface.h"
+#include "application.h"
+
+//==================================================================================================
+
+namespace plugin
+{
+    struct Plugin
+    {
+        uint64_t        id;
+        const char   *name;
+        InterfaceType type;
+
+        virtual Interface *get_interface() = 0;
+        virtual           ~Plugin       () = 0;
+    };
+}
+
+extern "C" plugin::Plugin* getInstance(plugin::App *app);
+
+#endif // PLUGIN_PLUGIN_H
